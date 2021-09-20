196ed8148b2406e4450857bad9a25bb6f542c0fe
jhuag <91040346+jhuag@users.noreply.github.com>
2021-09-19 21:54:28 -0500

    Update pull.php

---
+++ pull.php
@@ -0,0 +1,13 @@
+<?php
+
+$persona = [
+            "nombre" => "Pedro",
+            "apellidos" => "Sanchez",
+            "edad" => 50
+          ];
+          
+          foreach($persona as $valor){
+            echo  "$valor <br>";
+          }
+
+?>
