9468d5e3ee51d498386bff1a5f55b0b66789004e
jhuag <91040346+jhuag@users.noreply.github.com>
2021-09-19 21:38:25 -0500

    Create nav.php

---
+++ nav.php
@@ -0,0 +1,8 @@
+<header>
+  <ol>
+    <li><a href="#">Home</a></li>
+    <li><a href="#">Quienes Somos</a></li>
+    <li><a href="#">Nosotros</a></li>
+    <li><a href="#">Contactos</a></li>
+  </ol>
+</header>
